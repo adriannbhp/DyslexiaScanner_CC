40756cdbafb5cde854e02cb441a3186d34aa1504
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-12-10 21:09:04 +0700

    Update ( Berhasil Test API Local Postman)

--- main.py
+++ main.py
@@ -1,107 +1,54 @@
-# README
-# Hello everyone, in here I (Kaenova | Bangkit Mentor ML-20) 
-# will give you some headstart on createing ML API. 
-# Please read every lines and comments carefully. 
-# 
-# I give you a headstart on text based input and image based input API. 
-# To run this server, don't forget to install all the libraries in the
-# requirements.txt simply by "pip install -r requirements.txt" 
-# and then use "python main.py" to run it
-# 
-# For ML:
-# Please prepare your model either in .h5 or saved model format.
-# Put your model in the same folder as this main.py file.
-# You will load your model down the line into this code. 
-# There are 2 option I give you, either your model image based input 
-# or text based input. You need to finish functions "def predict_text" or "def predict_image"
-# 
-# For CC:
-# You can check the endpoint that ML being used, eiter it's /predict_text or 
-# /predict_image. For /predict_text you need a JSON {"text": "your text"},
-# and for /predict_image you need to send an multipart-form with a "uploaded_file" 
-# field. you can see this api documentation when running this server and go into /docs
-# I also prepared the Dockerfile so you can easily modify and create a container iamge
-# The default port is 8080, but you can inject PORT environement variable.
-# 
-# If you want to have consultation with me
-# just chat me through Discord (kaenova#2859) and arrange the consultation time
-#
-# Share your capstone application with me! 🥳
-# Instagram @kaenovama
-# Twitter @kaenovama
-# LinkedIn /in/kaenova
-
 ## Start your code here! ##
 
+from flask import Flask, request, jsonify
 import os
-import uvicorn
-import traceback
+import cv2
+import numpy as np
 import tensorflow as tf
+# from google.cloud import storage
 
-from pydantic import BaseModel
-from urllib.request import Request
-from fastapi import FastAPI, Response, UploadFile
-from utils import load_image_into_numpy_array
+app = Flask(__name__)
 
-# Initialize Model
-# If you already put yout model in the same folder as this main.py
-# You can load .h5 model or any model below this line
+model_path = os.path.join('models', 'dyslexia_scanner.h5')
+loaded_model = tf.keras.models.load_model(model_path)
 
-# If you use h5 type uncomment line below
-model = tf.keras.models.load_model('./dyslexia_scanner.h5')
-# If you use saved model type uncomment line below
-#model = tf.saved_model.load("./my_model_folder")
+# # Google Cloud Storage configuration
+# bucket_name = 'testing_data_cc'  # Replace with your GCS bucket name
+# storage_client = storage.Client()
 
-app = FastAPI()
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    resized_img = tf.image.resize(img, (256, 256))
+    return np.expand_dims(resized_img / 255, 0)
 
-# This endpoint is for a test (or health check) to this server
-@app.get("/")
-def index():
-    return "Hello world from ML endpoint!"
+# def upload_to_gcs(file_path, blob_name):
+#     bucket = storage_client.get_bucket(bucket_name)
+#     blob = bucket.blob(blob_name)
+#     blob.upload_from_filename(file_path)
 
-
-# If your model need image input use this endpoint!
-@app.post("/predict_image")
-def predict_image(uploaded_file: UploadFile, response: Response):
+@app.route('/test_dyslexia', methods=['POST'])
+def test_dyslexia():
     try:
-        # Checking if it's an image
-        if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
-            response.status_code = 400
-            return "File is Not an Image"
-        
-        # In here you will get a numpy array in "image" variable.
-        # You can use this file, to load and do processing
-        # later down the line
-        image = load_image_into_numpy_array(uploaded_file.file.read())
-        print("Image shape:", image.shape)
-        
-        # Step 1: (Optional, but you should have one) Do your image preprocessing
+        # Get the image file from the POST request
+        file = request.files['image']
+        file.save('uploaded_image.jpg')
 
-        image = tf.image.resize(image, (target_height, target_width))
-        
-        # Step 2: Prepare your data to your model
+        # Preprocess the image
+        processed_image = preprocess_image('uploaded_image.jpg')
 
-        image = image / 255.0
-        
-        # Step 3: Predict the data
-        # result = model.predict(...)
+        # Make predictions
+        prediction = loaded_model.predict(processed_image)
+        prediction_value = prediction[0][0]
 
-        result = model.predict(tf.expand_dims(image, axis=0))
-        
-        # Step 4: Change the result your determined API output
+        # Interpret the prediction
+        if prediction_value > 0.5:
+            diagnosis = "Dyslexia"
+        else:
+            diagnosis = "Normal"
 
-        api_output = {"prediction": result.tolist()}
-        return api_output
-        
-        return "Endpoint not implemented"
+        return jsonify({"diagnosis": diagnosis, "confidence": float(prediction_value)})
     except Exception as e:
-        traceback.print_exc()
-        response.status_code = 500
-        return "Internal Server Error"
+        return jsonify({"error": str(e)})
 
-
-# Starting the server
-# Your can check the API documentation easily using /docs after the server is running
-port = os.environ.get("PORT", 8080)
-print(f"Listening to http://0.0.0.0:{port}")
-uvicorn.run(app, host='0.0.0.0',port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
